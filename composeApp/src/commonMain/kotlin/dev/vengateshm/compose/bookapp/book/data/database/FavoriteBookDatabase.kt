d2df82576ba8a5f7f94ca4a51a7524ef77d8d0d3
vengateshm <vengateshm.13@gmail.com>
2024-12-02 17:31:30 +0530

    add annotation

--- composeApp/src/commonMain/kotlin/dev/vengateshm/compose/bookapp/book/data/database/FavoriteBookDatabase.kt
+++ composeApp/src/commonMain/kotlin/dev/vengateshm/compose/bookapp/book/data/database/FavoriteBookDatabase.kt
@@ -1,11 +1,13 @@
 package dev.vengateshm.compose.bookapp.book.data.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [BookEntity::class], version = 1)
 @TypeConverters(StringListTypeConverter::class)
+@ConstructedBy(BookDatabaseConstructor::class)
 abstract class FavoriteBookDatabase : RoomDatabase() {
     abstract val favoriteBookDao: FavoriteBookDao
 
